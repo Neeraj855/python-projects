db0a25293d89d6d474c4726e46f1c5a28a612d82
Neeraj855 <neerbalodi8@gmail.com>
2022-02-23 18:29:29 +0530

    Project1 : Email Valication using regEx

--- Email Validation/emailValidation.py
+++ Email Validation/emailValidation.py
@@ -1,35 +1,8 @@
-from curses.ascii import isalpha
+import re
+email_condition = "^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$"
+user_email = input('Eneter your email : ')
 
-
-email = input('Enter your Email : ')
-k,j,d=0,0,0
-
-
-if len(email)>=6:
-    if email[0].isalpha():
-        if ("@" in email) and (email.count('@') == 1 ):
-            if (email[-4]=='.') ^ (email[-3]=='.'):
-                for i in email:
-                    if i== i.isspace():
-                        k=1
-                    elif i.isalpha():
-                        if i == i.upper():
-                            j=1
-                    elif i.isdigit():
-                        continue
-                    elif i=='_' or i=='.' or i=='@':
-                        continue
-                    else:
-                        d=1
-                if k==1 or j==1 or d==1:
-                    print('Wrond Email 5')
-                else:
-                    print("Your Email is correct")
-            else:
-                print('Wrond Email 4')
-        else:
-            print('Wrong Email 3')
-    else:
-        print('Wrong Email 2')
+if re.search(email_condition,user_email):
+    print("Your email is correct")
 else:
-    print('Wrong Email 1')
+    print("Wrong Email")
